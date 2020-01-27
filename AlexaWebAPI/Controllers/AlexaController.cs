0c3bce09ace459fbcc41abe76ef90f7cbf27b031
lumf <luccas.monteiro@teleporting.com.br>
2020-01-27 01:30:38 -0300

    Add project files.

---
+++ AlexaWebAPI/Controllers/AlexaController.cs
@@ -0,0 +1,58 @@
+﻿using AlexaWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlexaController : ControllerBase
+    {
+        [HttpPost("myskill")]
+        public AlexaResponse MyCompanySkill(AlexaRequest request)
+        {
+            AlexaResponse response = null;
+
+            switch (request.Request.Intent.Name)
+            {
+                case "HelloIntent":
+                    response = HelloIntentHandler();
+                    break;
+                case "FavoriteBandIntent":
+                    response = FavoriteBandIntentHandler();
+                    break;
+                case "EndIntent":
+                case "AMAZON.CancelIntent":
+                case "AMAZON.StopIntent":
+                    response = EndIntentHandler();
+                    break;
+            }
+
+            return response;
+        }
+
+        private AlexaResponse FavoriteBandIntentHandler()
+        {
+            var favoriteBand = "Linkin Park.";
+
+            var response = new AlexaResponse("Sua banda favorita é " + favoriteBand);
+            response.Response.ShouldEndSession = false;
+            return response;
+        }
+
+        private AlexaResponse HelloIntentHandler()
+        {
+            var response = new AlexaResponse("Olar.");
+            response.Response.Reprompt.OutputSpeech.Text =
+                "You can tell me to say hello, what is my favorite band, or cancel to exit.";
+            response.Response.ShouldEndSession = false;
+            return response;
+        }
+
+        private AlexaResponse EndIntentHandler()
+        {
+            var response = new AlexaResponse("Espero ter ajudado, até logo.");
+            response.Response.ShouldEndSession = true;
+            return response;
+        }
+    }
+}
